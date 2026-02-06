2e1730d3d9c54303f7118554f8bfe933b63e9107
Tony Serrano <tonyserranodev@proton.me>
2026-02-05 22:40:09 -0600

    move leaf node class and tests into htmlnode.py and test_htmlnode.py, respectively

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,23 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode(tag: {self.tag}\nvalue: {self.value}\nchildren: {self.children}\nprops: {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, props)
+        self.tag = tag
+        self.value = value
+        self.props = props
+
+    def to_html(self):
+        if self.value is None or self.value == "":
+            raise ValueError("All leaf nodes must have a value")
+
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode(tag: {self.tag}\nvalue: {self.value}\nprops: {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -25,6 +25,37 @@ class TestHTMLNode(unittest.TestCase):
         actual = node.props_to_html()
 
         self.assertEqual(expected, actual)
+
+    def test_repr(self):
+        test_props = {"href": "https://www.google.com", "target": "_blank"}
+        node = HTMLNode(tag="a", value="This is a link", props=test_props)
+        expected = "HTMLNode(tag: a\nvalue: This is a link\nchildren: None\nprops: {'href': 'https://www.google.com', 'target': '_blank'})"
+        actual = node.__repr__()
+
+        self.assertEqual(expected, actual)
+
+    class TestLeafNode(unittest.TestCase):
+        def test_repr_no_props(self):
+            node = LeafNode(tag="p", value="this is some text")
+            expected = "LeafNode(tag: p\nvalue: this is some text\nprops: None)"
+            actual = node.__repr__()
+
+            self.assertEqual(expected, actual)
+
+        def test_repr_with_props(self):
+            test_props = {"href": "https://www.google.com", "target": "_blank"}
+            node = LeafNode(tag="a", value="this is a link", props=test_props)
+            expected = "LeafNode(tag: a\nvalue: this is a link\nprops: {'href': 'https://www.google.com', 'target': '_blank'})"
+            actual = node.__repr__()
+
+            self.assertEqual(expected, actual)
+
+        def test_tag_is_none(self):
+            node = LeafNode(value="this is some raw text", tag=None)
+            expected = "this is some raw text"
+            actual = node.to_html()
+
+            self.assertEqual(expected, actual)
 
 
 if __name__ == "__main__":
